b74a4a11020e8bff3accf4bac1d4d327a04729cd
Swanny Alves <swannykathllen@gmail.com>
2022-03-31 14:56:01 -0300

    chore: atualização no context

--- src/context/ContextCep.tsx
+++ src/context/ContextCep.tsx
@@ -7,15 +7,21 @@ import {
   useState,
 } from "react";
 import { DataCep } from "../services/api";
+
 interface Cep {
+  cep: string;
   logradouro: string;
   bairro: string;
   localidade: string;
   uf: string;
 }
 interface CepContextProps {
-  cep: Cep[];
+  cep: Cep;
   setCepData: Dispatch<SetStateAction<string | undefined>>;
+  cepError: boolean;
+  primeiraInteracao: boolean;
+  cepExist: boolean;
+  cepNumberNoExist: string;
 }
 
 interface CepContextProviderProps {
@@ -25,17 +31,44 @@ interface CepContextProviderProps {
 export const CepContext = createContext({} as CepContextProps);
 
 export const CepContextProvider = ({ children }: CepContextProviderProps) => {
-  const [cep, setCep] = useState<Cep[]>([]);
-  const [cepData, setCepData] = useState("");
+  const [cep, setCep] = useState();
+  const [cepData, setCepData] = useState<any>();
+  const [cepError, setCepError] = useState(false);
+  const [primeiraInteracao, setPrimeiraInteracao] = useState(true);
+  const [cepExist, setCepExist] = useState(false);
+  const [cepNumberNoExist, setCepNumberNoExist] = useState("");
 
   useEffect(() => {
-    DataCep(cepData).then((data) => {
-      setCep(data.data);
-    });
+    if (cepData === undefined || cepData === "" || cepData === null) {
+      setPrimeiraInteracao(true);
+      return;
+    } else {
+      DataCep(cepData).then((data) => {
+        setCepError(false);
+        if (data.data.erro === true) {
+          setCepError(true);
+          setCepExist(false);
+        } else {
+          setCep(data.data);
+          setCepError(false);
+          setCepExist(true);
+        }
+        setCepNumberNoExist(cepData);
+      });
+    }
   }, [cepData]);
 
   return (
-    <CepContext.Provider value={{ cep, setCepData }}>
+    <CepContext.Provider
+      value={{
+        cep,
+        setCepData,
+        cepError,
+        primeiraInteracao,
+        cepExist,
+        cepNumberNoExist,
+      }}
+    >
       {children}
     </CepContext.Provider>
   );
