ff5e6b34b0d7a706721b66252450206ad232ff47
Swanny Alves <swannykathllen@gmail.com>
2022-03-31 16:28:59 -0300

    feat: criação do component CardSearch

---
+++ src/components/CardSearch/index.tsx
@@ -0,0 +1,10 @@
+import { Search } from "../Search";
+import { CardSearchContainer } from "./style";
+
+export const CardSearch = () => {
+  return (
+    <CardSearchContainer>
+      <Search title={"Quer buscar um novo endereço?"} />
+    </CardSearchContainer>
+  );
+};
---
+++ src/components/CardSearch/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const CardSearchContainer = styled.div`
+    max-width: 545px;
+    height: 470px;
+    background-color: #FFFFFF;
+    border-radius: 10px;
+    padding: 60px;
+
+   
+`
