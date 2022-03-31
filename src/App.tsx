a8e4d39c0bb8fc6ec07ebbe51360ffccdf9e0c57
Swanny Alves <swannykathllen@gmail.com>
2022-03-31 15:30:56 -0300

    refactor: movendo Home para pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Home } from "./components/Home";
+import { Home } from "./pages/Home";
 import CepContextProvider from "./context/ContextCep";
 
 export const App = () => {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 import { useCepContext } from "../../hooks/useContextCep";
-import { CepNaoEncontrado } from "../CepNotFound";
-import { Result } from "../Result";
-import { Search } from "../Search";
+import { CepNaoEncontrado } from "../../components/CepNotFound";
+import { Result } from "../../components/Result";
+import { Search } from "../../components/Search";
 export const Home = () => {
   const { cep, primeiraInteracao, cepError, cepExist } = useCepContext();
   return (
