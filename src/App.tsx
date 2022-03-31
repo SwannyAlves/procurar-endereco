e5d01a3ff88ddb77aa60e10295202a2ec9f5d586
Swanny Alves <swannykathllen@gmail.com>
2022-03-31 15:33:28 -0300

    refactor: movendo GlobalStyle para o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import CepContextProvider from "./context/ContextCep";
 import { Home } from "./pages/Home";
+import { GlobalStyle } from "./styles/global";
 
 export const App = () => {
   return (
     <div className="App">
       <CepContextProvider>
+        <GlobalStyle />
         <Home />
       </CepContextProvider>
     </div>
